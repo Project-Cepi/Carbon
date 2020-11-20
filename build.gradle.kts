62f6dc93b36651ae139be23ed0154b936886c688
GraphPaper2 <63701505+GraphPaper2@users.noreply.github.com>
2020-11-20 12:07:38 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
+
+    // Apply the application plugin to add support for building a jar
+    java
+}
+
+repositories {
+    // Use jcenter for resolving dependencies.
+    jcenter()
+
+    // Use mavenCentral
+    maven(url = "https://repo1.maven.org/maven2/")
+    maven(url = "http://repo.spongepowered.org/maven")
+    maven(url = "https://libraries.minecraft.net")
+    maven(url = "https://jitpack.io")
+    maven(url = "https://jcenter.bintray.com/")
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation(kotlin("stdlib"))
+
+    // Use the Kotlin reflect library.
+    implementation(kotlin("reflect"))
+
+    // Compile Minestom into project
+    implementation("com.github.Minestom:Minestom:142b6a6")
+
+    // Implement Klaxon
+    implementation("com.beust:klaxon:5.0.1")
+
+    // OkHttp Client
+    implementation("com.squareup.okhttp3", "okhttp", "4.9.0")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
