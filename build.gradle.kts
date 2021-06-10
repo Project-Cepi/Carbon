96e4d3531a1630c408f525a0aa8ec7cd064996b1
LeoDog896 <LeoDog896@hotmail.com>
2021-06-10 15:20:39 -0400

    Fix build issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     compileOnly("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.2.1")
 
     // implement KStom
-    implementation("com.github.Project-Cepi:KStom:0129c5cfff")
+    compileOnly("com.github.Project-Cepi:KStom:0129c5cfff")
 }
 
 tasks.withType<Test> {
